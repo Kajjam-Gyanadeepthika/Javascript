449deea3faed04e3b2393b9e7bce60f83bdf95d5
Kajjam-Gyanadeepthika <gyanadeepthikakajjam@gmail.com>
2025-09-22 14:16:03 +0530

    bgcolor

---
+++ function.js
@@ -0,0 +1,17 @@
+// Function
+// a block of code to perform the multiple tasks
+// function add(){
+//   var a=67;
+//   var b =78;
+// console.log(a + b);
+// document.write(a + b);
+// }
+// add();
+const add = () =>{
+    var a= 89;
+    var b= 78;
+    document.write(a+b);
+    console.log(a+b);
+};
+
+add();
