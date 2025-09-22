101daaaa4087c01e5b7f1d45f05d811b7376bbf1
Kajjam-Gyanadeepthika <gyanadeepthikakajjam@gmail.com>
2025-09-22 11:04:20 +0530

    datatype

---
+++ app.js
@@ -0,0 +1,13 @@
+var a=67
+var a=90;
+let b=68;
+let b=54;
+
+console.log(a);
+console.log(b);
+console.log(`value of b is ${b}`);
+// Comment
+// datatypes
+// primitive-Number,string,boolean,null,undefined
+// nonprimitive-Array,object
+// typeof(),know the type of data
---
+++ datatype.js
@@ -0,0 +1,25 @@
+// datatypes
+// primitive-Number,string,boolean,null,undefined
+// nonprimitive-Array,object
+// typeof(),know the type of data
+// this,window commands to display the function of js
+a = 7897;
+console.log(typeof a);
+name = "Deepthi";
+console.log(typeof name);
+c = true;
+console.log(typeof c);
+data = null;
+console.log(typeof data);
+d = undefined;
+console.log(typeof d);
+// Non primitive
+array =[56,"Deepthi",true,null];
+console.log(typeof data);
+obj = {
+    name = "Deepthi",
+    age = "20",
+    dept = "AIML"
+};
+console.log(typeof data);
+
